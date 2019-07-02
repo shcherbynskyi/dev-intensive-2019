fa14e96f438a8aff75c0be9eb3ca06081c4db230
shcherbynskyi <bogdan.sherbinskiy@gmail.com>
2019-07-02 23:00:11 +0300

    update hometask #2

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -3,16 +3,14 @@ package ru.skillbranch.devintensive.models
 import java.util.*
 
 abstract class BaseMessage(
-    val id: String,
     val from: User?,
     val chat: Chat,
-    val isIncoming: Boolean = false,
-    val date: Date = Date()
+    val date: Date = Date(),
+    val isIncoming: Boolean = false
 ) {
 
     abstract fun formatMessage(): String
 
-    // Abstract factory to create messages instances
     companion object abstractFactory {
 
         var lastId = -1
@@ -20,8 +18,8 @@ abstract class BaseMessage(
         fun makeMessage(from: User?, chat: Chat, date: Date = Date(), payload: Any?, type: String = "text", isIncoming: Boolean = false): BaseMessage {
             lastId++
             return when (type) {
-                "image" -> ImageMessage("$lastId", from, chat, date = date, image = payload as String, isIncoming = isIncoming)
-                else -> TextMessage("$lastId", from, chat, date = date, text = payload as String, isIncoming = isIncoming)
+                "image" -> ImageMessage(from, chat, date = date, image = payload as String, isIncoming = isIncoming)
+                else -> TextMessage(from, chat, date = date, text = payload as String, isIncoming = isIncoming)
             }
         }
     }
