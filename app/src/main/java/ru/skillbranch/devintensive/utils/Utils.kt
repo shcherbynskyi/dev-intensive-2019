4132058015777d395bd6e6e299cf61fa215746ca
shcherbynskyi <bogdan.sherbinskiy@gmail.com>
2019-07-02 17:58:22 +0300

    hometask 2 last commit (pls be valid)
    • created function that parse user's fullname and returns pair
    • cover full hometask #2 with tests

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -3,6 +3,10 @@ package ru.skillbranch.devintensive.utils
 object Utils {
 
     fun parseFullName(fullName: String?): Pair<String?, String?> {
+
+        if (fullName.isNullOrBlank())
+            return Pair(null, null)
+
         val parts: List<String>? = fullName?.split(" ")
 
         val firstName = parts?.getOrNull(0)
