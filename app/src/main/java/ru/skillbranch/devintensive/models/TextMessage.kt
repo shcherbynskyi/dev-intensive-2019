5c86c0f5f1f663902573514267aa67f50b90977c
shcherbynskyi <bogdan.sherbinskiy@gmail.com>
2019-07-05 15:17:10 +0300

    another one update

--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -3,13 +3,14 @@ package ru.skillbranch.devintensive.models
 import ru.skillbranch.devintensive.extensions.humanizeDiff
 import java.util.*
 
-class TextMessage (
+class TextMessage(
+    id: String,
     from: User?,
     chat: Chat,
     isIncoming: Boolean = false,
     date: Date = Date(),
     var text: String?
-) : BaseMessage(from, chat, date, isIncoming) {
+) : BaseMessage(id, from, chat, isIncoming, date) {
 
     override fun formatMessage(): String = "${from?.firstName}" +
             "${ if (isIncoming) " получил" else " отправил" } сообщение \"$text\" ${date.humanizeDiff()}"
